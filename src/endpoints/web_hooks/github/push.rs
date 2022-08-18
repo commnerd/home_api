6401c8283f3a261cbdab8f90c3d33449a21843db
Michael J. Miller <commnerd@gmail.com>
2022-08-18 12:35:39 -0600

    Finished initial work on push request.

--- src/endpoints/web_hooks/github/push.rs
+++ src/endpoints/web_hooks/github/push.rs
@@ -1,6 +1,6 @@
-#[get("/push")]
-pub fn push() -> &'static str {
-    "Ok"
+#[post("/", data = "<input>")]
+pub fn push(input: String) -> String { // &'static str {
+    input
 }
 
 #[cfg(test)]
@@ -8,12 +8,15 @@ mod test {
     use rocket;
     use rocket::local::blocking::Client;
     use rocket::http::Status;
+    use std::fs;
 
     #[test]
     fn push() {
         let client = Client::untracked(crate::rocket()).expect("valid rocket instance");
-        let response = client.get("/").dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap(), "Ok".to_string());
+        let req = fs::read_to_string("src/endpoints/web_hooks/github/test/events/push.json");
+        
+        let resp = client.post("/push").body(req.as_ref().unwrap()).dispatch();
+        assert_eq!(resp.status(), Status::Ok);
+        assert_eq!(resp.into_string().unwrap(), req.unwrap());
     }
 }
