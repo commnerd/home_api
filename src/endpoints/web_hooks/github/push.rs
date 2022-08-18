db48b09814650bbc637b21bf14ef1acacddb8edc
Michael J. Miller <commnerd@gmail.com>
2022-08-18 13:09:53 -0600

    Started parsing data

--- src/endpoints/web_hooks/github/push.rs
+++ src/endpoints/web_hooks/github/push.rs
@@ -1,6 +1,10 @@
+use serde_json::{Result, Value};
+
 #[post("/", data = "<input>")]
-pub fn push(input: String) -> String { // &'static str {
-    input
+pub fn push(input: String) -> String {
+    let data: Value = serde_json::from_str(&input).unwrap();
+    let url: String = data["repository"]["clone_url"].to_string();
+    url
 }
 
 #[cfg(test)]
@@ -11,12 +15,12 @@ mod test {
     use std::fs;
 
     #[test]
-    fn push() {
+    fn push_result() {
         let client = Client::untracked(crate::rocket()).expect("valid rocket instance");
         let req = fs::read_to_string("src/endpoints/web_hooks/github/test/events/push.json");
         
         let resp = client.post("/push").body(req.as_ref().unwrap()).dispatch();
         assert_eq!(resp.status(), Status::Ok);
-        assert_eq!(resp.into_string().unwrap(), req.unwrap());
+        assert_eq!(resp.into_string().unwrap(), serde_json::to_string("https://github.com/commnerd/maikr.git").unwrap());
     }
 }
